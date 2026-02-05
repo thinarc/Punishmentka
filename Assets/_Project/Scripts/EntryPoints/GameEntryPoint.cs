e67cc446701a77b5a76921a5b2b91a7ce084ea76
ThinArc <145900005+thinarc@users.noreply.github.com>
2026-02-06 05:17:02 +0700

    вв

--- Assets/_Project/Scripts/EntryPoints/GameEntryPoint.cs
+++ Assets/_Project/Scripts/EntryPoints/GameEntryPoint.cs
@@ -10,13 +10,23 @@ namespace _Project.Scripts.EntryPoints
     [ExecuteInEditMode]
     public class GameEntryPoint : MonoBehaviour
     {
-        [SerializeField] private Light2D[] globals;
+        [SerializeField] private List<SceneState> states;
+        
+        [SerializeField, Space(5)] private Light2D[] globals;
         private float _targetIntensity;
 
         [SerializeField, Space(5)] private VolumeProfile[] profiles;
-        [SerializeField] private Volume volumeCamera;
-        
-        [SerializeField, Space(5)] private List<SceneState> states;
+        [SerializeField] private Volume[] volumeCamera;
+
+        private const string pixelCameraName = "Pixel Camera";
+
+        [SerializeField, Space(5)] private MeshRenderer worldView;
+        [SerializeField] private MeshRenderer worldViewRep;
+        [SerializeField] private Material[] renderMaterials;
+        [SerializeField] private Transform worldViewData;
+        [SerializeField] private Transform worldViewSmallData;
+
+        [SerializeField, Space(5)] private SimpleConfiner2D confiner;
 
         private int _lastOnStart = -100;
 
@@ -44,9 +54,57 @@ namespace _Project.Scripts.EntryPoints
             globals[0].intensity = Mathf.MoveTowards(globals[0].intensity, intensity, Time.deltaTime / 40f);
             globals[1].intensity = Mathf.MoveTowards(globals[1].intensity, intensity, Time.deltaTime / 40f);
             await UniTask.Delay(3200);
-            volumeCamera.profile = profiles[2];
+            volumeCamera[0].profile = profiles[2];
         }
         
+        private void ChangeScene()
+        {
+            states.ForEach(s =>
+            {
+                s.scene.SetActive(false);
+                if (!s.onStart) return;
+
+                s.scene.SetActive(true);
+                confiner.boundsCollider = s.scene.GetComponent<PolygonCollider2D>();
+                var vol = s.scene.name switch
+                {
+                    "Home" => 0,
+                    "Fantasy" => 1,
+                    "Final" => 3,
+                    _ => -100
+                };
+                var isChangedView = s.scene.name switch
+                {
+                    "Home" => false,
+                    "Fantasy" => false,
+                    "Final" => true,
+                    _ => false
+                };
+                var changeView = !isChangedView && volumeCamera[0].name != pixelCameraName || isChangedView && volumeCamera[0].name == pixelCameraName;
+                if (changeView) (volumeCamera[0], volumeCamera[1]) = (volumeCamera[1], volumeCamera[0]);
+                if (!volumeCamera[0].gameObject.activeInHierarchy) volumeCamera[0].gameObject.SetActive(true);
+                if (volumeCamera[1].gameObject.activeInHierarchy) volumeCamera[1].gameObject.SetActive(false);
+                volumeCamera[0].profile = profiles[vol];
+                volumeCamera[1].profile = null;
+                var renderMaterialsIndex = 0;
+                if (isChangedView) renderMaterialsIndex = 2;
+                if (worldView.sharedMaterial == renderMaterials[renderMaterialsIndex]) return;
+                worldView.sharedMaterial = renderMaterials[renderMaterialsIndex];
+                worldViewRep.sharedMaterial = renderMaterials[renderMaterialsIndex + 1];
+                var scale = worldViewData.localScale;
+                var pos = worldViewData.position;
+                if (isChangedView) scale = worldViewSmallData.localScale;
+                if (isChangedView) pos = worldViewSmallData.position;
+                if (worldView.transform.localScale != scale) worldView.transform.localScale = scale;
+                if (worldView.transform.position != pos) worldView.transform.position = pos;
+                var lens = worldViewData.GetComponent<WorldViewLens>().lens;
+                if (isChangedView) lens = worldViewSmallData.GetComponent<WorldViewLens>().lens;
+                var allCam = confiner.GetComponent<Camera>();
+                if (!Mathf.Approximately(allCam.orthographicSize, lens)) allCam.orthographicSize = lens;
+            });
+        }
+        
+#if UNITY_EDITOR
         private void OnValidate()
         {
             if (states.Count == 0 || states[0].scene == null)
@@ -73,22 +131,12 @@ namespace _Project.Scripts.EntryPoints
                 _lastOnStart = 0;
             }
             
-            states.ForEach(s =>
+            UnityEditor.EditorApplication.delayCall += () =>
             {
-                s.scene.SetActive(false);
-                if (!s.onStart) return;
-                
-                s.scene.SetActive(true);
-                var vol = s.scene.name switch
-                {
-                    "Home" => 0,
-                    "Fantasy" => 1,
-                    "Final" => 3,
-                    _ => -100
-                };
-                volumeCamera.profile = profiles[vol];
-            });
+                if (this != null) ChangeScene();
+            };
         }
+#endif
     }
 
     [System.Serializable]
---
+++ Assets/_Project/Scripts/EntryPoints/WorldViewLens.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+namespace _Project.Scripts.EntryPoints
+{
+    public class WorldViewLens : MonoBehaviour
+    {
+        public float lens;
+    }
+}
