def8d452a943815c25adb02d314b645035a89497
ThinArc <145900005+thinarc@users.noreply.github.com>
2026-02-08 05:08:44 +0700

    ddd

---
+++ Assets/_Project/Scripts/MiniGame/Blockblast.cs
@@ -0,0 +1,60 @@
+using UnityEngine;
+
+namespace _Project.Scripts.MiniGame
+{
+    public class Blockblast : MonoBehaviour
+    {
+        [SerializeField] private RectTransform source;
+        [SerializeField] private float cellSize;
+        
+        private Animator _anim;
+
+        private bool[,] _shape = new bool[8, 8];
+
+        private void Start()
+        {
+            _anim = GetComponent<Animator>();
+
+            RunGame();
+        }
+
+        public void RunGame()
+        {
+            UpdateModel();
+            UpdateView();
+        }
+
+        private void UpdateModel()
+        {
+
+        }
+
+        private void UpdateView()
+        {
+            _anim.enabled = true;
+            _anim.SetTrigger("Show");
+        }
+
+        private void OnDrawGizmos()
+        {
+            var topLeft = new Vector3(source.rect.xMin, source.rect.yMax);
+
+            for (var y = 0; y < 8; y++)
+            {
+                for (var x = 0; x < 8; x++)
+                {
+                    Gizmos.color = _shape[x, y] ? Color.red : Color.green;
+
+                    var pos = topLeft;
+                    pos += new Vector3(cellSize * 32, cellSize * -32);
+                    pos += new Vector3(x * cellSize * 64, y * cellSize * -64);
+                    pos = source.TransformPoint(pos);
+
+                    Gizmos.DrawCube(pos, new Vector3(cellSize, cellSize));
+                }
+            }
+        }
+
+        public void BlockAnim() => _anim.enabled = false;
+    }
+}
